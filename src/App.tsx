f4d357ae85729e98c3842d6e7fa28da45c4691e0
apileg <33292686+apileg@users.noreply.github.com>
2023-03-02 22:24:46 +0200

    ready to use

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import FavoritesPages from './pages/FavoritesPages'
+import HomePage from './pages/HomePage'
+import Navigation from './components/Navigation'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Navigation />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/favorites' element={<FavoritesPages />} />
+      </Routes>
+    </> 
+  )
 }
-
-export default App;
+  
